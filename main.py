e220fed82ed068802efa1db049cb2941c631c3fa
KylaLiwanag <kylacamilleliwanag@gmail.com>
2023-01-27 03:28:44 +0800

    Selection Sort

---
+++ main.py
@@ -0,0 +1,19 @@
+#SELECTION SORT
+
+def sort(nums):
+    for i in range(9):
+        minpos = i
+        for j in range(i,10):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+
+        print(nums)
+
+nums = [61,45,97,59,25,16,62,72,48,65]
+sort(nums)
+
+print(nums)
