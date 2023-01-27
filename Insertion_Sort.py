3eb7fcd62d684ee77ddb7843433ba27f30f799ec
KylaLiwanag <kylacamilleliwanag@gmail.com>
2023-01-27 09:13:44 +0800

    Insertion Sort print and layout changes

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -10,8 +10,6 @@ def sort(nums):
 
 nums = [61,45,97,59,25,16,62,72,48,65]
 print("---------------------INSERTION SORT--------------------")
-print("Unsorted List:", nums)
-print( )
+print("Unsorted List:",nums,"\n")
 sort(nums)
-print( )
-print("Sorted List:",nums)
+print("\nSorted List:",nums,"\n")
